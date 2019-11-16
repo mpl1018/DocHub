96a40f0de3cdceec7c33f7de8d073528cf537eb6
mpl1018 <44880660+mpl1018@users.noreply.github.com>
2019-11-16 15:52:23 +0200

    passing id parameter works

--- js/plans_f.js
+++ js/plans_f.js
@@ -1,4 +1,6 @@
 //Functions for plans page
+
+//drop down botton
 var dropdown_current = document.getElementById("dropdown-current");
 var dd_owner = document.getElementById("dd_owner"), dd_not_owner = document.getElementById("dd_not_owner"), 
 dd_all_plans = document.getElementById("dd_all_plans");
@@ -13,3 +15,7 @@ dd_not_owner.onclick = function () {
     dropdown_current.innerHTML = dd_not_owner.innerHTML;
 }
 
+//passing the id parameter
+var patientID  = localStorage.getItem('currentID'); 
+var righttopID = document.querySelector('#right_top_id');
+righttopID.innerHTML = "Patient id: " + patientID;
